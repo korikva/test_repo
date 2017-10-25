d34e447c67757470582aa4d3415b495661ab73fb
Korik <korik.va@gmail.com>
2017-10-24 22:30:00 -0400

    added a cuadratic

--- Plot.R
+++ Plot.R
@@ -5,4 +5,7 @@ b <- -2
 a <- 100
 y <- b*x + a
 
+y2 <- b*x^2 + b*x + a
+
 plot(x, y, xlab= "Independent variable", ylab= "Dependent variable", type = "l")
+plot(x, y2, xlab= "Independent variable", ylab= "Dependent variable", type = "l")
